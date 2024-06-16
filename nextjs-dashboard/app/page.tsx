0b3bd0a1ab9a6664c2d93409e6ffc45410ab2b25
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2024-06-16 21:29:44 +0100

    chore: update page

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,29 +1,35 @@
 
+import 'reset-css';
+import { Poppins } from 'next/font/google'
+import '@/styles/global.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { Hero } from '@/components/hero/hero';
 import ServiceCards from '@/components/cards/service-cards';
 import CancellationCard from '@/components/cards/cancellation-card';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import BlogCarousel from '@/components/carousels/blog-carousel';
 import DestinationCarousel from '@/components/carousels/destination-carousel';
 import Navbar from '@/components/navbar/navbar';
 
-import 'reset-css';
-import { Poppins } from 'next/font/google'
-import '@/styles/global.scss';
+
 
 const poppins = Poppins({ subsets: ['latin'], weight: ["200", "300", "400", "500", "600", "700"] });
 export default function Page() {
   return (
-    <main className={poppins.className}>
-
+    <>
       <Navbar />
-      <Hero backgroundImage="/images/background-image.jpg"  subtitle="Buy your flights now and pay later" title="Search and book flights to Australia" trustScoreText="TrustScore 4.5 | 6,500 reviews" />
-      <ServiceCards />
-      <DestinationCarousel />
-      <CancellationCard heading="Cancellation protection" text="During these uncertain times we recommend adding Cancellation protection to you booking" />
-      <BlogCarousel />
+      <main className={poppins.className}>
 
-    </main>
+        <Hero backgroundImage="/images/background-image.jpg" subtitle="Buy your flights now and pay later" title="Search and book flights to Australia" trustScoreText="TrustScore 4.5 | 6,500 reviews" />
+        <ServiceCards />
+        <DestinationCarousel />
+        <CancellationCard heading="Cancellation protection" text="During these uncertain times we recommend adding Cancellation protection to you booking" />
+        <BlogCarousel />
+
+      </main>
+    </>
+
   );
 }
